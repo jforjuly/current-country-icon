eb17a106863ab8858ad84c3558c9af6e14b2f4ce
Igor N <jforjuly@outlook.com>
2021-09-11 22:55:29 +0300

    [FORMAT] Formatted with prettier

--- background.js
+++ background.js
@@ -1,23 +1,23 @@
-const apiUrl = "https://json.geoiplookup.io/"
+const apiUrl = "https://json.geoiplookup.io/";
 const iconMappings = {
-  "RU": "ru.png",
-  "DE": "de.png",
-  "NL": "nl.png"
-}
-const defaultIcon = "un.png"
-const iconsFolder = "icons"
+  RU: "ru.png",
+  DE: "de.png",
+  NL: "nl.png",
+};
+const defaultIcon = "un.png";
+const iconsFolder = "icons";
 const changeIcon = (jsonResponse) => {
-  const countryCode = jsonResponse["country_code"]
-  const newIcon = iconMappings[countryCode] || defaultIcon
-  chrome.browserAction.setIcon({ path: iconsFolder + "/" + newIcon })
-}
+  const countryCode = jsonResponse["country_code"];
+  const newIcon = iconMappings[countryCode] || defaultIcon;
+  chrome.browserAction.setIcon({ path: iconsFolder + "/" + newIcon });
+};
 
-const interval = 1000
+const interval = 1000;
 const updater = () => {
   fetch(apiUrl)
     .then(function (response) {
       return response.json();
     })
     .then(changeIcon);
-}
-setInterval(updater, 1000)
+};
+setInterval(updater, interval);
